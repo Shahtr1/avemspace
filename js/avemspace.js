a34511124561ecef1cd4d161432e45dc8c0a71c4
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2019-05-22 16:23:11 +0530

    first_page change

---
+++ js/avemspace.js
@@ -0,0 +1,9 @@
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+window.onscroll = () => {
+  const nav = document.querySelector('#navbar');
+  if(this.scrollY <= 300) nav.className = ''; else nav.className = 'scroll';
+};
