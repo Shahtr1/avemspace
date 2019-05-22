224e49f2a67635eb254b434c16606d5d3e0ff0e8
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2019-05-22 23:39:14 +0530

    about changes

--- js/avempace.js
+++ js/avempace.js
@@ -5,10 +5,6 @@ function topFunction() {
 
 window.onscroll = () => {
   const nav = document.querySelector('#navbar');
-  if(this.scrollY <= 300) nav.className = ''; else nav.className = 'scroll';
-};
-
-window.onscroll = () => {
-  const nav = document.querySelector('#navbar1');
   if(this.scrollY <= 100) nav.className = ''; else nav.className = 'scroll';
 };
+
